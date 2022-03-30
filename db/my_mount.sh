875633a524526b671755b3809d356c1925d5e28d
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2022-03-30 09:56:04 +0300

    Update my_mount.sh

--- db/my_mount.sh
+++ db/my_mount.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-sudo s3fs ike-bucket-small /mnt/my-bucket -o use_cache=/tmp/bucket-cache -o passwd_file=${HOME}/.passwd-s3fs -o ibm_iam_auth -o allow_other
-sudo /etc/init.d/mysqld restart
+s3fs ike-bucket-small /mnt/my-bucket -o use_cache=/tmp/bucket-cache -o passwd_file=${HOME}/.passwd-s3fs -o ibm_iam_auth -o allow_other
+/etc/init.d/mysqld restart
