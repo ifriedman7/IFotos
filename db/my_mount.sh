dc5bd850bf14c016225a25c67145b5bee1f4bb6e
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2022-04-03 14:03:35 +0300

    Update my_mount.sh

--- db/my_mount.sh
+++ db/my_mount.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 s3fs ike-bucket-small /mnt/my-bucket -o use_cache=/tmp/bucket-cache -o passwd_file=${HOME}/.passwd-s3fs -o allow_other
 #/usr/sbin/mysqld --daemonize
+touch /var/log/mysqld.log
+ln -sf /dev/stdout /var/log/mysqld.log 
+#ln -sf /dev/stderr /var/log/mysql/log.err
 exec "$@"
