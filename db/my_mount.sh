c7d7775104241c8c7467b28dee0eaac24609da3b
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2022-05-19 16:01:59 +0300

    Change datadir

--- db/my_mount.sh
+++ db/my_mount.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-s3fs ike-bucket-small /mnt/my-bucket -o use_cache=/tmp/bucket-cache -o passwd_file=${HOME}/.passwd-s3fs -o allow_other
+#Using s3provider... Next step: remove s3fs command for kube system. Or leave in for non-Kube.
+s3fs ike-bucket-small /var/s3fs -o use_cache=/tmp/bucket-cache -o passwd_file=${HOME}/.passwd-s3fs -o allow_other
 #/usr/sbin/mysqld --daemonize
 touch /var/log/mysqld.log
 ln -sf /dev/stdout /var/log/mysqld.log 
